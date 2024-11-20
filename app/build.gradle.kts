aa0674e56fe41ca74c4c3391bc750ed23b4c6674
ebruaeo <ebruaeo@hotmail.com>
2024-11-20 17:49:29 +0300

    pre-requisities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.sharedpreferences"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.sharedpreferences"
         minSdk = 24
